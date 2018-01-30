4fe306388c209fb20b8b2711007406ce569d56cc
merradi <marouane.erradi@gmail.com>
2018-01-30 11:43:45 +0100

    review codacy

--- src/main/java/bank/account/Customer.java
+++ src/main/java/bank/account/Customer.java
@@ -1,12 +1,8 @@
 package bank.account;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Customer {
 
     private String client_name;
-    private List<Account> accounts = new ArrayList<>();
 
 	public Customer(String client_name) {
 		this.client_name = client_name;
